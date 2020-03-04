48b8e00124dae5c5b6786ad8df3436ea11493252
Admin <>
2020-03-04 15:09:12 +0300

    create favorites friend view controller

---
+++ VKMessenger/VKMessenger/Screens/FavoritesFriends/FavoritesFriendsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesFriendsViewController.swift
+//  VKMessenger
+//
+//  Created by Admin on 04/03/2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesFriendsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

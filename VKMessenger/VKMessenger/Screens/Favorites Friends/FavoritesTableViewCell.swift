145b78d5735666cf2ac98436a5a1d96846f9362f
Admin <>
2020-03-05 19:08:15 +0300

    add favorites friends view controller

---
+++ VKMessenger/VKMessenger/Screens/Favorites Friends/FavoritesTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FavoritesTableViewCell.swift
+//  VKMessenger
+//
+//  Created by Admin on 05/03/2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImage: UIImageView! 
+}
